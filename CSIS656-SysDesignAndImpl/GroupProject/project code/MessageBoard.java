c3edc1333bf84103d460d0c5b9c0daffe7c157dd
chris on Lenovo <follybeachris@hotmail.com>
2014-04-13 18:58:42 -0400

    added 656 applets to javascript code part

---
+++ CSIS656-SysDesignAndImpl/GroupProject/project code/MessageBoard.java
@@ -0,0 +1,20 @@
+package applet;
+
+import java.applet.Applet;
+
+public class MessageBoard extends Applet {
+
+	public MessageBoard() {
+
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		String at = e.getActionCommand();
+		if (at.equals("Send"))
+		{
+			
+		}
+		
+	}
+
+}
